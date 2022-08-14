32116d8013e83c397dd03ea1ef67658f575a6e2e
Christofer Nolander <christofer.nolander@gmail.com>
2022-08-14 18:08:35 +0200

    fix double-free for zero-size-type

--- tests/derive.rs
+++ tests/derive.rs
@@ -48,14 +48,21 @@ fn readme() {
 
 #[test]
 fn non_copy_with_drop() {
-    use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::atomic::{AtomicBool,AtomicUsize, Ordering};
+
+    static MAY_DROP: AtomicBool = AtomicBool::new(false);
     static DROP_COUNT: AtomicUsize = AtomicUsize::new(0);
 
     struct Droppable;
     impl Drop for Droppable {
         fn drop(&mut self) {
             eprintln!("drop");
-            DROP_COUNT.fetch_add(1, Ordering::SeqCst);
+            let count = DROP_COUNT.fetch_add(1, Ordering::SeqCst);
+
+            // if we are the first drop, but dropping shouldn't happen yet, panic
+            if count == 0 && !MAY_DROP.load(Ordering::SeqCst) {
+                panic!("early drop");
+            }
         }
     }
 
@@ -63,14 +70,13 @@ fn non_copy_with_drop() {
     #[derive(DynStruct)]
     struct MyDynamicType {
         pub boxed: Droppable,
-        pub dynamic: [u32],
+        pub dynamic: [Droppable],
     }
 
-    let foo: Box<MyDynamicType> = MyDynamicType::new(Droppable, 1..8);
+    let foo: Box<MyDynamicType> = MyDynamicType::new(Droppable, [Droppable, Droppable, Droppable]);
     assert_eq!(DROP_COUNT.load(Ordering::SeqCst), 0, "creating DynStruct should not result in drop");
 
-    assert_eq!(&foo.dynamic, &[1, 2, 3, 4, 5, 6, 7]);
+    MAY_DROP.store(true, Ordering::SeqCst);
     drop(foo);
-
-    assert_eq!(DROP_COUNT.load(Ordering::SeqCst), 1, "dropping DynStruct should result in drop");
+    assert_eq!(DROP_COUNT.load(Ordering::SeqCst), 4, "dropping DynStruct should result in drop");
 }
